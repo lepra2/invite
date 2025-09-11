1952cd089c8e4576ebb5b6f3535fc15dade8288f
lepra2 <lepra2@yahoo.com>
2025-09-11 12:41:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("codeForm").addEventListener("submit", async (e) => {
   const code = document.getElementById("code").value;
 
   try {
-    // 1. Проверка кодового слова
+    // 1. Проверка кодового слова же
     const checkRes = await fetch("https://api.leprosorium2.ru/check-code", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -18,7 +18,7 @@ document.getElementById("codeForm").addEventListener("submit", async (e) => {
       return;
     }
 
-    // 2. Отправка email на воркер
+    // 2. Отправка email
     const emailRes = await fetch("https://api.leprosorium2.ru/email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
